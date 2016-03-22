95a0c4018321faf5f572227726fbc686dec0fbdd
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-22 11:10:31 -0700

    finish piping

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -3,6 +3,8 @@ import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
 import { NewTaskComponent } from './new-task.component';
+import {DonePipe} from './done.pipe';
+
 
 
 
@@ -10,14 +12,21 @@ import { NewTaskComponent } from './new-task.component';
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  pipes: [DonePipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   template: `
-  <task-display *ngFor="#currentTask of taskList"
+  <select (change)="onChange($event.target.value)"class="filter">
+    <option value="all">Show All</option>
+    <option value="done">Show Done</option>
+    <option value="notDone" selected="selected">Show Not Done</option>
+  </select>
+  <task-display *ngFor="#currentTask of taskList | done:filterDone"
     (click)="taskClicked(currentTask)"
     [class.selected]="currentTask === selectedTask"
     [task]="currentTask">
   </task-display>
-  <edit-task-details *ngIf="selectedTask" [task]="selectedTask"></edit-task-details>
+  <edit-task-details *ngIf="selectedTask" [task]="selectedTask">
+  </edit-task-details>
   <new-task> (onSubmitNewTask)="createTask($event)"> </new-task>
   `
 })
@@ -26,11 +35,12 @@ export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
+
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
   }
@@ -38,5 +48,9 @@ export class TaskListComponent {
     this.taskList.push(
       new Task(description, this.taskList.length)
     );
+  }
+
+  onChange(filterOption) {
+    this.filterDone = filterOption;
   }
 }
